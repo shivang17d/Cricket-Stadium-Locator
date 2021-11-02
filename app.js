4e0852ce136d4f500c95ba4dbcc7ea6a69e5630f
Shivang Gupta <86548591+sgdevelop@users.noreply.github.com>
2021-11-03 00:18:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const myMap = L.map('map').setView([22.9074872, 79.07306671], 5);
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attribution =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by coder\'s gyan with ❤️';
+const tileLayer = L.tileLayer(tileUrl, { attribution });
+tileLayer.addTo(myMap);
+
+
+function generateList() {
+    const ul = document.querySelector('.list');
+    stadiumList.forEach((stadium) => {
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        const a = document.createElement('a');
+        const p = document.createElement('p');
+        a.addEventListener('click', () => {
+            flyToStore(stadium);
+        });
+        div.classList.add('stadium-item');
+        a.innerText = stadium.properties.name;
+        a.href = '#';
+        p.innerText = stadium.properties.address;
+
+        div.appendChild(a);
+        div.appendChild(p);
+        li.appendChild(div);
+        ul.appendChild(li);
+    });
+}
+
+generateList();
+
+function makePopupContent(stadium) {
+    return `
+    <div>
+        <h4>${stadium.properties.name}</h4>
+        <p>${stadium.properties.address}</p>
+    </div>
+  `;
+}
+
+function onEachFeature(feature, layer) {
+    layer.bindPopup(makePopupContent(feature), { closeButton: false, offset: L.point(0, -8) });
+}
+
+var myIcon = L.icon({
+    iconUrl: 'marker.png',
+    iconSize: [30, 40]
+});
+
+const stadiumsLayer = L.geoJSON(stadiumList, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+        return L.marker(latlng, { icon: myIcon });
+    }
+});
+stadiumsLayer.addTo(myMap);
+
+function flyToStore(stadium) {
+    const lat = stadium.geometry.coordinates[1];
+    const lng = stadium.geometry.coordinates[0];
+    myMap.flyTo([lat, lng], 14, {
+        duration: 3
+    });
+    setTimeout(() => {
+        L.popup({ closeButton: false, offset: L.point(0, -8) })
+            .setLatLng([lat, lng])
+            .setContent(makePopupContent(stadium))
+            .openOn(myMap);
+    }, 3000);
+}
---
+++ stadium.js
@@ -0,0 +1,101 @@
+const stadiumList = [{
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [72.59721079262498, 23.092618065430216]
+        },
+        "properties": {
+            "name": "Narendra Modi Stadium",
+            "address": "Stadium Rd, Motera, Ahmedabad, Gujarat 380005, India",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [88.34300700795768, 22.56536106914556]
+
+        },
+        "properties": {
+            "name": "Eden Gardens",
+            "address": "Maidan, B.B.D. Bagh, Kolkata, West Bengal 700021, India",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [72.82574253884873, 18.939035451707202]
+        },
+        "properties": {
+            "name": "Wankhede Stadium",
+            "address": "Vinoo Mankad Rd, Churchgate, Mumbai, Maharashtra 400020, India",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [81.01705136136157, 26.8122315396137]
+        },
+        "properties": {
+            "name": "Ekana Cricket Stadium",
+            "address": "Ekana Sportz City, Sector 7, Amar Shaheed Path, Gomti Nagar, Lucknow, Uttar Pradesh 226010, India",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [73.70646245325905, 18.674783452881677]
+        },
+        "properties": {
+            "name": "Maharashtra Cricket Association Stadium",
+            "address": "Mumbai - Pune Hwy, Gahunje, Maharashtra 412101, India",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [80.2792708197057, 13.06336842951331]
+        },
+        "properties": {
+            "name": "M.A. Chidambaram Stadium",
+            "address": "Iyya Mudali St, Chintadripet, Chennai, Tamil Nadu 600002, India",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [77.5996498, 12.979924791330477]
+        },
+        "properties": {
+            "name": "M Chinnaswamy Stadium",
+            "address": "Cubbon Rd, Shivaji Nagar, Bengaluru, Karnataka 560001, India",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [78.55004124659611, 17.407590899221635]
+        },
+        "properties": {
+            "name": "Rajiv Gandhi International Cricket Stadium",
+            "address": "RGI Stadium Rd, Uppal, Hyderabad, Telangana 500039, India",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [77.2431091246107, 28.638087768128884]
+        },
+        "properties": {
+            "name": "Arun Jaitley Stadium",
+            "address": "Jawaharlal Nehru Marg, Feroze Shah Kotla, Raj Ghat, New Delhi, Delhi 110002, India",
+        }
+    }
+]
