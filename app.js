1b83652a26631ce1cd69a8a8678b6419eeb047f8
Shivang Gupta <86548591+shivanggupta24@users.noreply.github.com>
2021-11-07 09:43:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const myMap = L.map('map').setView([22.9074872, 79.07306671], 5);
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const attribution =
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by coder\'s gyan with ❤️';
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by SHIVANG GUPTA with ❤️';
 const tileLayer = L.tileLayer(tileUrl, { attribution });
 tileLayer.addTo(myMap);
 
